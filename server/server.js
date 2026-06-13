e9161a01222dbe3ee9f60a7907f58803a42588dc
Adz-Nelo <adz.nelo155@gmail.com>
2026-06-14 01:36:44 +0800

    testing deployment w/ prisma

--- server/server.js
+++ server/server.js
@@ -1,15 +1,19 @@
-import express from 'express';
-import 'dotenv/config';
-import cors from 'cors';
-import { clerkMiddleware } from '@clerk/express'
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import { clerkMiddleware } from "@clerk/express";
+import { inngestServe } from "./inngest/index.js";
 
 const app = express();
 
 app.use(express.json());
-app.use(cors()); 
+app.use(cors());
 app.use(clerkMiddleware());
 
-app.get('/', (req, res) => res.send('Server is live!'));
+app.get("/", (req, res) => res.send("Server is live!"));
+
+// Inngest HTTP endpoint
+app.use("/api/inngest", inngestServe);
 
 const PORT = process.env.PORT || 5000;
 
